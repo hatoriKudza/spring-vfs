22ab52a18aad69c0b3fd1779dfef6b6e05cfa79d
Bauyrzhan Tursyn <bauyrzhan.tursyn@uco.kz>
2023-07-10 22:34:35 +0600

    init

---
+++ src/main/java/kz/btursyn/springvfs/SpringVfsApplication.java
@@ -0,0 +1,13 @@
+package kz.btursyn.springvfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVfsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/btursyn/springvfs/SpringVfsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.btursyn.springvfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
